e1acab8c1067cbc867794a206bcb725031be00c0
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-09 19:12:04 +0700

    Fixing Master Mahasiswa

--- resources/views/superadmin/master/mahasiswa/form-action.blade.php
+++ resources/views/superadmin/master/mahasiswa/form-action.blade.php
@@ -1,2 +1,8 @@
 <a href="/mahasiswa/{{ $model->slug }}/edit" class="btn btn-sm btn-warning"><i class="bi bi-pen"></i></a>
 <a href="/mahasiswa/{{ $model->slug }}" class="btn btn-sm btn-primary"><i class="bi bi-eye"></i></a>
+
+<form action="/mahasiswa/{{ $model->slug }}" class="d-inline" method="post">
+    @method('put')
+    @csrf
+    <button name="status" value="checked" class="btn btn-sm btn-{{ $model->status > 0 ? 'danger' : 'success' }}" ><i class="{{ $model->status > 0 ? 'bi bi-x-circle' : 'bi bi-check-circle' }}"></i></button>
+</form>
