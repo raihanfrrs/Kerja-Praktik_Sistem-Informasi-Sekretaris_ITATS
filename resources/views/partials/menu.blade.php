25517e824b4e4b665e0a4f04225c90768206cc7f
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-16 09:53:17 +0700

    adding job and role

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -70,7 +70,7 @@
     <a class="nav-link {{ request()->is('master/*') ? '' : 'collapsed' }}" data-bs-target="#master-super-nav" data-bs-toggle="collapse" href="#">
         <i class="bi bi-collection"></i><span>Data Master</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
-    <ul id="master-super-nav" class="nav-content collapse {{ request()->is('mahasiswa', 'mahasiswa/*', 'dosen', 'dosen/*', 'surat', 'surat/*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
+    <ul id="master-super-nav" class="nav-content collapse {{ request()->is('mahasiswa', 'mahasiswa/*', 'dosen', 'dosen/*', 'surat', 'surat/*', 'role', 'role/*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
         <li>
             <a href="{{ url('mahasiswa') }}" class="{{ request()->is('mahasiswa', 'mahasiswa/*') ? 'active' : '' }}">
                 <i class="bi bi-circle"></i><span>Mahasiswa</span>
@@ -86,13 +86,11 @@
                 <i class="bi bi-circle"></i><span>Surat</span>
             </a>
         </li>
+        <li>
+            <a href="{{ url('role') }}" class="{{ request()->is('role' ,'role/*') ? 'active' : '' }}">
+                <i class="bi bi-circle"></i><span>Role</span>
+            </a>
+        </li>
     </ul>
-</li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="index.html">
-    <i class="bi bi-inboxes"></i>
-    <span>Riwayat</span>
-    </a>
 </li>
 @endif
