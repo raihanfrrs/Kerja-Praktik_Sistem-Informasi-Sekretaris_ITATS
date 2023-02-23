7b4bb1903b62516c05d2928535420b63d30bc682
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-23 10:07:03 +0700

    fixing layout session

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\Dosen;
+use App\Models\JenisSurat;
 use App\Models\Mahasiswa;
-use Illuminate\Http\Request;
 
 class LayoutController extends Controller
 {
     public function index()
     {
-        session(['deactivate' => Mahasiswa::where('status', 'deactivated')->count() + Dosen::where('status', 'deactivated')->count()]);
+        session(['deactivate' => Mahasiswa::where('status', 'deactivated')->count() 
+                                + Dosen::where('status', 'deactivated')->count() 
+                                + Role::where('status', 'deactivated')->count() 
+                                + JenisSurat::where('status', 'deactivated')->count()]);
 
         return view('welcome');
     }
