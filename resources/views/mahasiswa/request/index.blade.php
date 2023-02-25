2fbafe266dabad7cacf348e51500a71f7ca30868
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-25 09:30:33 +0700

    adding search form in navbar

--- resources/views/mahasiswa/request/index.blade.php
+++ resources/views/mahasiswa/request/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('section')
 <div class="card overflow-auto-mx-2">
-    test
+    <div class="row gx-0 my-5 mx-5">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card with titles, buttons, and links</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <p class="card-text"><a href="#" class="btn btn-primary w-100">Button</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
