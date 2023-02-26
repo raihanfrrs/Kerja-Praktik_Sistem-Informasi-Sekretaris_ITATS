8c2a0e1b0c8773b11151d0e4c928f1a634e9b096
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-26 11:58:42 +0700

    fixing overview surat

--- resources/views/mahasiswa/request/data.blade.php
+++ resources/views/mahasiswa/request/data.blade.php
@@ -1,13 +1,13 @@
 @foreach ($surats as $surat)
     <div class="col-md-3">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body" >
                 <h5 class="card-title text-capitalize">{{ $surat->name }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $surat->jenis_surat->jenis }}</h6>
                 <p class="card-text">{{ Str::limit(strip_tags($surat->description), 100) }}</p>
                 <div class="row">
                 <div class="col-6">
-                    <a href="#" class="btn btn-secondary w-100"><i class="bi bi-list-columns"></i> Details</a>
+                    <a href="#" class="btn btn-secondary w-100" id="detail-surat-button" data-id="{{ $surat->slug }}"><i class="bi bi-list-columns"></i> Details</a>
                 </div>
                 <div class="col-6">
                     <a href="#" class="btn btn-primary w-100"><i class="bi bi-send-plus"></i> Request</a>
