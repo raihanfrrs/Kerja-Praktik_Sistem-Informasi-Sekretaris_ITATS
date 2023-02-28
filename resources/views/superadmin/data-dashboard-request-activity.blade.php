769a029364d4b96d2ee9983eceac0529ae3f36a5
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2023-02-28 15:24:59 +0700

    adding alert to dashboard request activity

--- resources/views/superadmin/data-dashboard-request-activity.blade.php
+++ resources/views/superadmin/data-dashboard-request-activity.blade.php
@@ -1,15 +1,21 @@
-@foreach ($requests as $request)
-<div class="post-item clearfix">
-    @if ($request->mahasiswa->image)
-            <img src="{{ asset('storage/'. $request->mahasiswa->image) }}" class="img-fluid rounded" alt="{{ $request->mahasiswa->image }}">
-        @else
-            <img src="{{ asset('/') }}assets/img/profile-img.jpg" alt="{{ $request->mahasiswa->image }}">
-        @endif
-    <h4><a href="/mahasiswa/{{ $request->mahasiswa->slug }}">{{ $request->mahasiswa->name }}</a></h4>
-    <p>Requesting {{ $request->detail_request->count() }} Surat.
-        <br>
-        <span>{{ $request->created_at->diffForHumans() }}.</span>
-    </p>
-</div>
-<hr>
-@endforeach
+@if ($requests->count() == 0)
+    <div class="alert alert-danger" role="alert">
+        No activity requests are currently in active!
+    </div>
+@else
+    @foreach ($requests as $request)
+    <div class="post-item clearfix">
+        @if ($request->mahasiswa->image)
+                <img src="{{ asset('storage/'. $request->mahasiswa->image) }}" class="img-fluid rounded" alt="{{ $request->mahasiswa->image }}">
+            @else
+                <img src="{{ asset('/') }}assets/img/profile-img.jpg" alt="{{ $request->mahasiswa->image }}">
+            @endif
+        <h4><a href="/mahasiswa/{{ $request->mahasiswa->slug }}">{{ $request->mahasiswa->name }}</a></h4>
+        <p>Requesting {{ $request->detail_request->count() }} Surat.
+            <br>
+            <span>{{ $request->created_at->diffForHumans() }}.</span>
+        </p>
+    </div>
+    <hr>
+    @endforeach
+@endif
